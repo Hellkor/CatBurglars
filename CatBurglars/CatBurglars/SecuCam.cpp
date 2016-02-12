0e6e8fe6b632c1c057f140ecba5ccc91671a4db0
samwetha <a15samwe@HS-S-D20315.hs.local>
2016-02-12 13:16:34 +0100

    Added SecuCam
    Fixed shit

--- CatBurglars/CatBurglars/SecuCam.cpp
+++ CatBurglars/CatBurglars/SecuCam.cpp
@@ -1,52 +1,81 @@
 #include "secuCam.h"
 
-
-
-secuCam::secuCam(int channel, gridvector coords, sf::CircleShape shape) :
+secuCam::secuCam(int channel, gridvector coords, sf::Texture *texture,int range) :
 mChannel(channel),
 mCoords(coords){
-
-	sf::CircleShape triangle(60, 5);
 
 	mPosition.x = mCoords.x * 64;
 	mPosition.y = mCoords.y * 64;
 
-	//mSprite.setTexture(*texture);
-
+	mSprite.setTexture(*texture);
 	mSprite.setPosition((sf::Vector2f)mPosition);
+
+
+	int width = 0;
+
+	for (int i = 1; i < range; i++){
+
+		mVision.push_back(new gridvector(mCoords.x,mCoords.y + i));
+		
+		for (int j = 0; j < width; j++){
+			mVision.push_back(new gridvector(mCoords.x + j, mCoords.y + i));
+			mVision.push_back(new gridvector(mCoords.x + -j, mCoords.y + i));
+		}
+
+			width + 1;
+	}
+
+
 }
 
 void secuCam::Update(float dt){
-	if (Channels::isChannelActive(mChannel)){
-		isOn = false;
-	}
+	//	if (Channels::isChannelActive(mChannel)){ 	isOn = false;	}
 }
 
 void secuCam::connectToChannel(int channel){
 	mChannel = channel;
 }
 
+
+
+
 sf::Vector2i secuCam::GetPosition(){
 	return mPosition;
 }
+
+
 
 gridvector secuCam::getCoords(){
 	return mCoords;
 }
 
+sf::Vector2i secuCam::getDirection(){
+	return mDirection;
+}
 
 
-void secuCam::Render(sf::RenderWindow *mainWindow){
-	mainWindow->draw(camShape);
-	
+
+
+void secuCam::Render(sf::RenderWindow *window){
+	window->draw(mSprite);
+	for each (gridvector *v in mVision){
+		mSprite.setPosition(v->x * 64, v->y * 64);
+		window->draw(mSprite);
+	}
 }
 
 bool secuCam::isInteracting(){
 	return false;
 }
 
+bool secuCam::isSolid(){
+	return true;
+}
 
 
-
-
-
+/*
+bool secuCam::isOn(){
+if (mChannel.isActive){
+mOn != isOn
+}
+*/ //växlar mellan två texturer
--- CatBurglars/CatBurglars/SecuCam.h
+++ CatBurglars/CatBurglars/SecuCam.h
@@ -1,38 +1,40 @@
+
 #pragma once
 
 #include "Recievers.h"
 #include "Channels.h"
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
 
 class secuCam : public Recievers{
 
 public:
 
-	secuCam(int channel, gridvector coords, sf::CircleShape shape/*sf::Texture *texture*/);
+	secuCam(int channel, gridvector coords, sf::Texture *texture,int range);
 	//~secuCam();
 	virtual void connectToChannel(int channelID);
 	virtual void Update(float dt);
 	virtual void Render(sf::RenderWindow *window);
 	virtual sf::Vector2i GetPosition();
+	virtual sf::Vector2i getDirection();
 	virtual gridvector getCoords();
 	//virtual bool isSolid(); //kanske ska va solid, kanske inte, hör med design 
 	virtual bool isInteracting();
-	 bool isOn;
+	//bool isOn;
 
-	 //virtual sf::FloatRect getBoundingBox();
-	sf::CircleShape camShape;
+	virtual bool isSolid();
 
 
 private:
 
-
+	std::vector<gridvector*> mVision;
 	int mChannel;
 	bool mOn;
 	//bool mSolid; //samma som med isSolid();
 	sf::Vector2i mPosition;
+	sf::Vector2i mDirection;
 	gridvector mCoords;
 	sf::Sprite mSprite;
+
 
 
 };
